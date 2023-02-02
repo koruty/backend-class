624a5a04884aa61f9d45dfc561042fa6b4c03b16
koruty <124110592+koruty@users.noreply.github.com>
2023-02-02 17:45:08 +0900

    4day

---
+++ day4/out.js
@@ -0,0 +1,27 @@
+function 빨래하다(옷들, 물, 세제, 시간) {
+    console.log("1. 먼지를 털자.")
+    console.log("2. 물에 집어넣는다.")
+    console.log("3. 비누를 바른다")
+    console.log("4. 오물조물 비빈다.")
+    console.log("5. 물에 여러번 헹군다.")
+    console.log("6. 짠다.")
+    console.log("7. 넌다.")
+}
+function 청소하다(청소기, 밀다, 먼지통비우기) {
+    console.log("1. 청소기를 든다")
+    console.log("2. 청소기로 방을 민다")
+    console.log("3. 먼지통을 비운다")
+    console.log("4. 청소기를 거치한다")
+} //alert로 해서 함수실행 시켜도 팝업창으로 출력됨!
+
+function 여행하다(계획을세우다, 떠나다, 놀다, 복귀하다) {
+    console.log("1. 계획을 세운다")
+    console.log("2. 떠난다")
+    console.log("3. 논다.")
+    console.log("4. 복귀하다")
+}
+function 쉬다(눕다, 먹다, 자다) {
+    console.log("1. 쉬다")
+    console.log("2. 먹다")
+    console.log("3. 자다")
+}
